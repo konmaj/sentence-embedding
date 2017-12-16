e9f0115a44fe41ea107e326821dff8d53d5b621e
Konrad Majewski <km371194@students.mimuw.edu.pl>
2017-12-16 15:48:54 +0100

    GloVe embeddings downloader

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt /tmp/
 
 RUN pip3 install -r /tmp/requirements.txt
 
-ENTRYPOINT ["python3"]
+ENTRYPOINT ["python3", "-u"]
--- src/sent_emb/downloader/downloader.py
+++ src/sent_emb/downloader/downloader.py
@@ -1,11 +1,34 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+from shutil import unpack_archive
 
 
+DOWNLOAD_DIR = Path('/', 'opt', 'resources')
 
 
 def get_datasets():
     print('Ensure that datasets exist and download if not')
 
-def get_embeddings():
-    print('Ensure that embeddings exist and download if not')
 
+GLOVE_URL = 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
+GLOVE_SIZE = 2.2 #GB
+GLOVE_DIR = DOWNLOAD_DIR.joinpath('embeddings', 'glove')
+GLOVE_ZIP = GLOVE_DIR.joinpath(Path(GLOVE_URL).name)
+
+
+def get_embeddings():
+    print('Checking for embeddings:')
+    
+    if not GLOVE_DIR.exists():
+        GLOVE_DIR.mkdir(parents=True, exist_ok=True)
+        
+        print('  GloVe embeddings not found. Downloading', GLOVE_URL, '(' + str(GLOVE_SIZE) + 'GB)')
+        urlretrieve(GLOVE_URL, str(GLOVE_ZIP))
+        
+        print('  Unpacking', GLOVE_ZIP)
+        unpack_archive(str(GLOVE_ZIP), extract_dir=str(GLOVE_DIR))
+        
+        GLOVE_ZIP.unlink()
+    else:
+        print('  Found GloVe embeddings')
 
