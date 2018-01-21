8b03f39f5bd63b08dfeb05f72715aec5dce32bc7
Konrad Majewski <km371194@students.mimuw.edu.pl>
2018-01-21 20:41:19 +0100

    Running evaluation from any directory

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 ./get_embeddings.sh || exit $?
 ./get_stanfordNLP.sh || exit $?
 
@@ -7,8 +9,8 @@ docker build -t sentence-embedding ..
 
 
 docker run \
-   -v $(pwd)/../src/sent_emb:/opt/sent_emb \
-   -v $(pwd)/../resources/:/opt/resources \
+   -v "$(pwd)/../src/sent_emb:/opt/sent_emb" \
+   -v "$(pwd)/../resources/:/opt/resources" \
    -w /opt/sent_emb/evaluation \
    -e PYTHONPATH=/opt/ \
    sentence-embedding\
