ef0b752075bf994517331537d35c98ae6aa996cd
Marek Zbysinski <marek.zbysinski@gmail.com>
2018-03-07 20:13:34 +0100

    Script run mode for statistics
    You can specify to run statistics as an argument of script.

--- src/sent_emb/evaluation/evaluation.py
+++ src/sent_emb/evaluation/evaluation.py
@@ -11,7 +11,7 @@ from preprocessing import PreprocessingNltk, PreprocessingStanford
 
 
 # All available evaluation methods.
-RUN_MODES = ['STS', 'test']
+RUN_MODES = ['STS', 'stats', 'test']
 
 # All available algorithms with their constructors.
 ALGORITHMS = {
@@ -39,38 +39,49 @@ parser.add_argument('--alg-kwargs', help='specify JSON with kwargs to init metho
 args = parser.parse_args()
 
 
-alg_kwargs = json.loads(args.alg_kwargs)
+# Run proper mode
 
-if args.run_mode == 'test':
-    args.tokenizer = 'ignored'
-    args.algorithm = 'ignored'
-    args.alg_kwargs = 'ignored'
-else:
+downloader.get_datasets()
+
+if args.run_mode == 'STS':
+    alg_kwargs = json.loads(args.alg_kwargs)
+
+    params_msg = '''
+Script params
+    run-mode: {0}
+    tokenizer: {1}
+    algorithm: {2}
+    alg-kwargs: {3}
+'''.format(args.run_mode, args.tokenizer, args.algorithm, alg_kwargs)
+    print(params_msg)
+
     if args.algorithm is None:
         print('\nERROR: You have to specify algorithm in every run mode except the \'test\' mode.')
         print('Example: ./run_evaluation.sh Doc2Vec')
         sys.exit(1)
 
-params_msg = '''
-Evaluation params
-      run-mode: {0}
-     tokenizer: {1}
-     algorithm: {2}
-    alg-kwargs: {3}
-'''.format(args.run_mode, args.tokenizer, args.algorithm, alg_kwargs)
-print(params_msg)
-
-
-# Run proper evaluation function
-
-downloader.get_datasets()
-
-if args.run_mode == 'STS':
     tokenizer = TOKENIZERS[args.tokenizer]()
     algorithm = ALGORITHMS[args.algorithm](**alg_kwargs)
     sts.eval_sts_all(algorithm, tokenizer)
 
+elif args.run_mode == 'stats':
+    params_msg = '''
+Script params
+    run-mode: {0}
+    tokenizer: {1}
+'''.format(args.run_mode, args.tokenizer)
+    print(params_msg)
+
+    tokenizer = TOKENIZERS[args.tokenizer]()
+    all_statistics(tokenizer)
+
 elif args.run_mode == 'test':
+    params_msg = '''
+Script params
+    run-mode: {0}
+'''.format(args.run_mode)
+    print(params_msg)
+
     algorithm = ALGORITHMS['Doc2Vec']() # take any algorithm
     for token_name, token_ctor in TOKENIZERS.items():
         print('\nTesting tokenizer {}...\n'.format(token_name))
@@ -83,6 +94,3 @@ elif args.run_mode == 'test':
 
 else:
     assert False
-
-
-#all_statistics(PreprocessingNltk())
