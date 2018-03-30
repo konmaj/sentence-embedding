f76028a9ea7c409ece3785d1c3a9e07b7ba080b5
Marek Zbysinski <marek.zbysinski@gmail.com>
2018-03-30 15:48:06 +0200

    Organized requirements
    scripts/run_docker.sh uses CPU-version of tensorflow, whereas
    scripts/run_virtualenv.sh uses GPU one.
    
    requirements.txt is partitioned into 4 separate files to improve
    usage of docker's cached images.
    
    Project's REAMDE.md and .gitignore are updated.

--- scripts/run_virtualenv.sh
+++ scripts/run_virtualenv.sh
@@ -7,18 +7,26 @@ SCRIPT_DIR=$(pwd)
 PROJECT_DIR=${SCRIPT_DIR}/..
 
 PIP_ENV=${PROJECT_DIR}/py35
-REQ_FILE=${PROJECT_DIR}/requirements.txt
+REQ_DIR=${PROJECT_DIR}/requirements
 SCRIPT_TO_RUN=${PROJECT_DIR}/src/sent_emb/evaluation/evaluation.py
+
+# Prepare virtualenv
 
 if [ ! -d "$PIP_ENV" ]; then
     virtualenv -p python3.5 "${PIP_ENV}"
 fi
 
 source ${PIP_ENV}/bin/activate
-pip install -r "${REQ_FILE}"
+
+pip3 install -r "${REQ_DIR}/1_math.txt"
+pip3 install -r "${REQ_DIR}/2_nlp.txt"
+pip3 install -r "${REQ_DIR}/3_ml.txt"
+pip3 install -r "${REQ_DIR}/4_other.txt"
+pip3 install tensorflow-gpu==1.6.0
+
 python3.5 -c "import nltk; nltk.download('punkt')"
 
-# run script with environment variables
+# Run script with environment variables
 PYTHONPATH=${PROJECT_DIR}/src \
 RESOURCES_DIR=${PROJECT_DIR}/resources \
 python3.5 ${SCRIPT_TO_RUN} "$@"
