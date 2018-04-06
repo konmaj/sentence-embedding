ae4f13deca38a8f362541f9c7c0b8b3279b2712c
Marek Zbysinski <marek.zbysinski@gmail.com>
2018-04-06 14:10:59 +0200

    Autoencoder use GloVe class now
    Before autoencoder used glove_cropped.txt file from previous evaluation
    and crashed when this file contained 50-dimensional vectors.

--- src/sent_emb/algorithms/simple_autoencoder.py
+++ src/sent_emb/algorithms/simple_autoencoder.py
@@ -1,43 +1,32 @@
 import numpy as np
 from keras.layers import Dense, BatchNormalization
-from keras.models import Sequential
-from keras import backend as K, Input, Model
+from keras import Input, Model
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sent_emb.evaluation.model import BaseAlgorithm
 from sent_emb.algorithms.unknown import UnknownVector
-from sent_emb.algorithms.glove_utility import GLOVE_DIM, GLOVE_FILE, read_file
+from sent_emb.algorithms.glove_utility import GloVe
 
 
-def parse_sents(sents, unknown, maxlen):
-    where = {}
-    words = set()
+GLOVE_DIM = 300
 
+
+def parse_sents(sents, word_embedding, maxlen):
     result = np.zeros((sents.shape[0], maxlen, GLOVE_DIM), dtype=np.float)
+
+    word_to_vec = word_embedding.embeddings(sents)
 
     for ids, sent in enumerate(sents):
         for idw, word in enumerate(sent):
-            if word != '':
-                if word not in where:
-                    where[word] = []
-                where[word].append((ids, idw))
-
-    def process(word, vec, _):
-        words.add(word)
-        unknown.see(word, vec)
-        if word in where:
-            for (ids, idw) in where[word]:
-                result[ids, idw] = vec
-
-    read_file(GLOVE_FILE, process)
+            result[ids, idw] = word_to_vec[word]
 
     result = result.reshape((result.shape[0], result.shape[1]*result.shape[2]))
 
     return result
 
 
-def buildModel(target_dim, input_dim, output_dim):
+def build_model(target_dim, input_dim, output_dim):
     inputs = Input(shape=(input_dim,))
     x = Dense(500, activation='relu')(inputs)
     x = BatchNormalization()(x)
@@ -63,24 +52,24 @@ class SimpleAutoencoder(BaseAlgorithm):
         '''
         self.encoder = None
         self.model = None
-        self.unknown = unknown
+        self.word_embedding = GloVe(unknown)
         self.maxlen = maxlen
         self.target_dim = target_dim
 
-    def get_resources(self, _):
-        pass
+    def get_resources(self, task):
+        self.word_embedding.get_resources(task)
 
     def fit(self, sents):
-        x_train = parse_sents(sents, self.unknown, self.maxlen)
+        x_train = parse_sents(sents, self.word_embedding, self.maxlen)
 
         vectorizer = CountVectorizer(preprocessor=(lambda line: ' '.join(line).lower()), binary=True)
         y_train = vectorizer.fit_transform(sents).toarray()
 
-        self.encoder, self.model = buildModel(self.target_dim, self.maxlen * GLOVE_DIM, y_train.shape[1])
+        self.encoder, self.model = build_model(self.target_dim, self.maxlen * GLOVE_DIM, y_train.shape[1])
 
         self.model.fit(x_train, y_train, epochs=1, batch_size=64, shuffle=True)
 
     def transform(self, sents):
-        x_test = parse_sents(sents, self.unknown, self.maxlen)
+        x_test = parse_sents(sents, self.word_embedding, self.maxlen)
 
         return self.encoder.predict(x_test)
--- src/sent_emb/evaluation/model.py
+++ src/sent_emb/evaluation/model.py
@@ -45,7 +45,7 @@ class WordEmbedding(ABC):
 
     @abstractmethod
     def embeddings(self, sents):
-        """"
+        """
         Main function, which computes vectors for all words used in task
         :param sents: list of sentences
         :return: dictionary of embeddings for words used in sentences
@@ -54,28 +54,28 @@ class WordEmbedding(ABC):
 
 
 class BaseAlgorithm(ABC):
-    '''
+    """
     Base abstract class, which represents algorithms, which compute sentence embeddings.
 
     Note - following call:
         SubclassName()
     should yield valid (but maybe not optimally tuned) object of SubclassName
     (for the purpose of smoketest).
-    '''
+    """
 
     @abstractmethod
     def get_resources(self, task):
-        '''
+        """
         Called once before all evaluations. Prepares external resources
         (downloads data, crops files for better performace).
         :param task: Task object
-        '''
+        """
         pass
 
 
     @abstractmethod
     def fit(self, sents):
-        '''
+        """
         Prepares 'self' object on training data.
 
         Should be called before first call to transform() method.
@@ -87,15 +87,15 @@ class BaseAlgorithm(ABC):
 
         sents: sequence of sentences as in sent_emb.evaluation.sts.read_sts_input() function's result
         returns: None
-        '''
+        """
         pass
 
     @abstractmethod
     def transform(self, sents):
-        '''
+        """
         Computes embeddings of given sentences.
 
         sents: sequence of sentences as in sent_emb.evaluation.sts.read_sts_input() function's result
         returns: numpy 2-D array of sentence embeddings
-        '''
+        """
         pass
