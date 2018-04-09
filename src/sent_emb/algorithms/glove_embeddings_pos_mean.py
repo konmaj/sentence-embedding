87d7c0c08921b7b7d39d1460ac794427044f7e29
Konrad Majewski <km371194@students.mimuw.edu.pl>
2018-04-09 03:52:25 +0200

    Added grouping POS tags, different weights possible

--- src/sent_emb/algorithms/glove_embeddings_pos_mean.py
+++ src/sent_emb/algorithms/glove_embeddings_pos_mean.py
@@ -11,7 +11,19 @@ POS_TAGS = ['ADJ', 'ADP', 'ADV', 'CONJ', 'NOUN', 'PRON', 'PROPN', 'VERB']
 N_TAGS = len(POS_TAGS)
 
 class GlovePosMean(BaseAlgorithm):
-    def __init__(self, unknown=UnknownVector(300)):
+    def __init__(self, unknown=UnknownVector(300),
+                 tag_groups=[(POS_TAGS, 1), (['NOUN', 'VERB'], 0.5)],
+                 simple_mean_coeff=0.01):
+        """
+        :param tag_groups: list of pairs (tags list, weight)
+            for each such group there is created seperate mean with given
+            weight
+        :param simple_mean_coeff: multiplier of simple mean of all words
+            always set as non-zero, otherwise some sentences may receive
+            embeddings that are all 0s.
+        """
+        self.tag_groups = tag_groups
+        self.simple_mean_coeff = simple_mean_coeff
         self.word_embeddings = GloVe(unknown)
         self.simple_mean = GloveMean(unknown)
         self.nlp = spacy.load('en')
@@ -27,10 +39,11 @@ class GlovePosMean(BaseAlgorithm):
         wordvec = self.word_embeddings.embeddings(sents)
 
         n_sents = len(sents)
+        n_groups = len(self.tag_groups)
         glove_dim = self.word_embeddings.get_dim()
 
-        result = np.zeros((n_sents, N_TAGS, glove_dim), dtype=np.float)
-        count = np.zeros((n_sents, N_TAGS, 1))
+        result = np.zeros((n_sents, n_groups, glove_dim), dtype=np.float)
+        count = np.zeros((n_sents, n_groups, 1))
 
         for idx, sent in enumerate(sents):
             sent_string = ' '.join(sent)
@@ -38,11 +51,13 @@ class GlovePosMean(BaseAlgorithm):
             for word in doc:
                 pos = word.pos_
                 if pos in POS_TAGS and word.text in wordvec:
-                    pos_id = POS_TAGS.index(pos)
-                    result[idx][pos_id] += wordvec[word.text]
-                    count[idx][pos_id][0] += 1
+                    for idg, group in enumerate(self.tag_groups):
+                        tags, wei = group
+                        if pos in tags:
+                            result[idx][idg] += wei * wordvec[word.text]
+                            count[idx][idg][0] += 1
 
         result /= np.maximum(count, [1])
 
-        return np.concatenate([0.01 * simple_embeddings,
-                               result.reshape((n_sents, N_TAGS * glove_dim))], axis=1)
+        return np.concatenate([self.simple_mean_coeff * simple_embeddings,
+                               result.reshape((n_sents, n_groups * glove_dim))], axis=1)
